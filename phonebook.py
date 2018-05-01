b5f2bc66658c5994d944adcd3fd0b4e50e431ffe
Matthew Freeman <matthew.freeman821@gmail.com>
2018-05-01 13:08:40 -0400

    Working with dictionaries

---
+++ phonebook.py
@@ -0,0 +1,19 @@
+phoneBookDict = {
+    'Alice': '703-493-1834',
+    'Bob': '857-384-1234',
+    'Elizabeth': '484-584-293'
+}
+#1
+print(phoneBookDict['Elizabeth'])
+
+#2
+phoneBookDict['Kareem'] = '938-489-1234'
+
+#3
+phoneBookDict['Alice'] = None
+
+#4
+phoneBookDict['Bob'] = '968-345-2345'
+
+#5
+print(phoneBookDict)
